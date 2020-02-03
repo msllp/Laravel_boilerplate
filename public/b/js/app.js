c695a6775d35c0e904a0bef656158e6bcb577d99
msllp <48418814+msllp@users.noreply.github.com>
2020-02-03 14:00:58 +0530

    update

--- public/b/js/app.js
+++ public/b/js/app.js
@@ -3646,6 +3646,171 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue?vue&type=script&lang=js&":
+/*!*******************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue?vue&type=script&lang=js& ***!
+  \*******************************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  name: "profile",
+  props: {
+    'msData': {
+      type: Object,
+      required: true
+    }
+  },
+  data: function data() {
+    return {
+      msUserData: this.msData // msUserData:{
+      //     Username:'maxirooney',
+      //     sex:'female',
+      //     email:'user@company.com',
+      //     fname:'Mitul',
+      //     lname:'Patel',
+      //     phone:'9662611234',
+      //     plan:{
+      //
+      //         name:'Test',
+      //         limits:[
+      //             {
+      //                 vName:'Products',
+      //                 limit:5,
+      //                 usage:1,
+      //
+      //
+      //             },
+      //             {
+      //                 vName:'Invoice / Month',
+      //                 limit:5,
+      //                 usage:1,
+      //             },
+      //             {
+      //                 vName:'Purchase / Month',
+      //                 limit:5,
+      //                 usage:1,
+      //             }
+      //         ]
+      //     }
+      //
+      // }
+
+    };
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./vendor/msllp/core/src/Views/core/B/s/js/MS/msDashboard.vue?vue&type=script&lang=js&":
 /*!*****************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./vendor/msllp/core/src/Views/core/B/s/js/MS/msDashboard.vue?vue&type=script&lang=js& ***!
@@ -79565,6 +79730,176 @@ render._withStripped = true
 
 /***/ }),
 
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue?vue&type=template&id=f14fc10a&scoped=true&":
+/*!***********************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue?vue&type=template&id=f14fc10a&scoped=true& ***!
+  \***********************************************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", { staticClass: "flex flex-wrap" }, [
+    _c("div", { staticClass: "container" }, [
+      _c("div", { staticClass: "profileBox" }, [
+        _c("div", { staticClass: "profileBox-container" }, [
+          _c("div", { staticClass: "profileBox-left-box" }, [
+            _c("div", { staticClass: "profileBox-left-icon" }, [
+              _c("svg", { staticClass: "profileBox-left-icon-user" }, [
+                _c("use", {
+                  attrs: {
+                    "xlink:href":
+                      "#msicon-svg-user-" + _vm.msUserData.sex + "-1"
+                  }
+                })
+              ])
+            ]),
+            _vm._v(" "),
+            _c("div", { staticClass: "profileBox-left-username" }, [
+              _vm._v(_vm._s(_vm.msUserData.Username))
+            ])
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "profileBox-right-box" }, [
+            _c("table", { staticClass: "profileBox-right-box-table" }, [
+              _c("tr", [
+                _c("th", [_vm._v("Name")]),
+                _vm._v(" "),
+                _c("td", [
+                  _vm._v(
+                    _vm._s(_vm.msUserData.fname) +
+                      " " +
+                      _vm._s(_vm.msUserData.lname)
+                  )
+                ])
+              ]),
+              _vm._v(" "),
+              _c("tr", [
+                _c("th", [_vm._v("Email")]),
+                _vm._v(" "),
+                _c("td", [_vm._v(_vm._s(_vm.msUserData.email))])
+              ]),
+              _vm._v(" "),
+              _c("tr", [
+                _c("th", [_vm._v("Contact no.")]),
+                _vm._v(" "),
+                _c("td", [_vm._v(_vm._s(_vm.msUserData.phone))])
+              ]),
+              _vm._v(" "),
+              _c("tr", [
+                _c("th", [_vm._v("Active Plan")]),
+                _vm._v(" "),
+                _c("td", [_vm._v(_vm._s(_vm.msUserData.plan.name))])
+              ])
+            ])
+          ])
+        ])
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "w-full flex flex-fill flex-wrap" }, [
+        _c("div", { staticClass: "usageBox" }, [
+          _c(
+            "table",
+            { staticClass: "usageBox-box-table" },
+            [
+              _vm._m(0),
+              _vm._v(" "),
+              _vm._l(_vm.msUserData.plan.limits, function(row) {
+                return _c("tr", [
+                  _c("th", { staticClass: "text-left" }, [
+                    _vm._v(_vm._s(row.vName))
+                  ]),
+                  _vm._v(" "),
+                  _c("td", [
+                    _vm._v(
+                      _vm._s(row.usage) +
+                        "/" +
+                        _vm._s(row.limit) +
+                        "\n\n                        "
+                    )
+                  ]),
+                  _vm._v(" "),
+                  _c("td", [
+                    _c("progress", {
+                      attrs: { max: row.limit },
+                      domProps: { value: row.usage }
+                    })
+                  ])
+                ])
+              })
+            ],
+            2
+          )
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "planUpgradeBox" }, [
+          _c(
+            "table",
+            { staticClass: "planUpgradeBox-table" },
+            [
+              _vm._m(1),
+              _vm._v(" "),
+              _vm._l(_vm.msUserData.plan.limits, function(row) {
+                return _c("tr", [
+                  _c("th", { staticClass: "text-left" }, [
+                    _vm._v(_vm._s(row.vName))
+                  ]),
+                  _vm._v(" "),
+                  _c("td", [
+                    _vm._v(_vm._s(row.limit) + "\n                        ")
+                  ]),
+                  _vm._v(" "),
+                  _c("td", [_vm._v("  + 10  ")])
+                ])
+              }),
+              _vm._v(" "),
+              _vm._m(2)
+            ],
+            2
+          )
+        ])
+      ])
+    ])
+  ])
+}
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("tr", { staticClass: "usageBox-box-table-h" }, [
+      _c("th", { attrs: { colspan: "3" } }, [_vm._v("Active Plan Details")])
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("tr", { staticClass: "planUpgradeBox-table-h" }, [
+      _c("th", { attrs: { colspan: "3" } }, [_vm._v("Upgrade Your Plan")])
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { attrs: { colspan: "planUpgradeBox-table-f" } }, [
+      _c("span", [_vm._v("Upgrade Now")])
+    ])
+  }
+]
+render._withStripped = true
+
+
+
+/***/ }),
+
 /***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./vendor/msllp/core/src/Views/core/B/s/js/MS/msDashboard.vue?vue&type=template&id=b5711320&scoped=true&":
 /*!*********************************************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./vendor/msllp/core/src/Views/core/B/s/js/MS/msDashboard.vue?vue&type=template&id=b5711320&scoped=true& ***!
@@ -93930,6 +94265,75 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue":
+/*!**********************************************************************!*\
+  !*** ./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue ***!
+  \**********************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _profilePage_vue_vue_type_template_id_f14fc10a_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./profilePage.vue?vue&type=template&id=f14fc10a&scoped=true& */ "./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue?vue&type=template&id=f14fc10a&scoped=true&");
+/* harmony import */ var _profilePage_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./profilePage.vue?vue&type=script&lang=js& */ "./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../../../../../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _profilePage_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _profilePage_vue_vue_type_template_id_f14fc10a_scoped_true___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _profilePage_vue_vue_type_template_id_f14fc10a_scoped_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  false,
+  null,
+  "f14fc10a",
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue?vue&type=script&lang=js&":
+/*!***********************************************************************************************!*\
+  !*** ./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue?vue&type=script&lang=js& ***!
+  \***********************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_profilePage_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../../../../../../../../node_modules/babel-loader/lib??ref--4-0!../../../../../../../../../../../node_modules/vue-loader/lib??vue-loader-options!./profilePage.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_profilePage_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue?vue&type=template&id=f14fc10a&scoped=true&":
+/*!*****************************************************************************************************************!*\
+  !*** ./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue?vue&type=template&id=f14fc10a&scoped=true& ***!
+  \*****************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_profilePage_vue_vue_type_template_id_f14fc10a_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../../../../../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../../../../../../../../node_modules/vue-loader/lib??vue-loader-options!./profilePage.vue?vue&type=template&id=f14fc10a&scoped=true& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue?vue&type=template&id=f14fc10a&scoped=true&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_profilePage_vue_vue_type_template_id_f14fc10a_scoped_true___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_profilePage_vue_vue_type_template_id_f14fc10a_scoped_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
+
+/***/ }),
+
 /***/ "./vendor/msllp/core/src/Views/core/B/s/js/MS/msDashboard.vue":
 /*!********************************************************************!*\
   !*** ./vendor/msllp/core/src/Views/core/B/s/js/MS/msDashboard.vue ***!
@@ -94455,7 +94859,8 @@ Vue.component('mssidenav', __webpack_require__(/*! ./MS/C/msSideNav.vue */ "./ve
 Vue.component('newtab', __webpack_require__(/*! ./MS/C/msNewTab.vue */ "./vendor/msllp/core/src/Views/core/B/s/js/MS/C/msNewTab.vue")["default"]);
 Vue.component('salesdashboard', __webpack_require__(/*! ./MS/D/Sales.vue */ "./vendor/msllp/core/src/Views/core/B/s/js/MS/D/Sales.vue")["default"]);
 Vue.component('mscalc', __webpack_require__(/*! ./MS/C/msCalc.vue */ "./vendor/msllp/core/src/Views/core/B/s/js/MS/C/msCalc.vue")["default"]);
-Vue.component('mslogin', __webpack_require__(/*! ./MS/msLoginPage.vue */ "./vendor/msllp/core/src/Views/core/B/s/js/MS/msLoginPage.vue")["default"]); //Components
+Vue.component('mslogin', __webpack_require__(/*! ./MS/msLoginPage.vue */ "./vendor/msllp/core/src/Views/core/B/s/js/MS/msLoginPage.vue")["default"]);
+Vue.component('profile', __webpack_require__(/*! ./MS/P/profilePage.vue */ "./vendor/msllp/core/src/Views/core/B/s/js/MS/P/profilePage.vue")["default"]); //Components
 
 
 var store = {
